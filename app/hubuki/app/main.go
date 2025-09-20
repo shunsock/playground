aa672af7ece370f94d5b04ea622a51d339522f71
shunsock <s.tsuchiya.business@gmail.com>
2025-09-21 00:50:43 +0900

    feature: create the first api

--- app/hubuki/app/main.go
+++ app/hubuki/app/main.go
@@ -1,19 +1,44 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"log"
 	"net/http"
 
-	"github.com/shunsock/hubuki/internal/server"
+	"github.com/danielgtaylor/huma/v2"
+	"github.com/danielgtaylor/huma/v2/adapters/humachi"
+	"github.com/go-chi/chi/v5"
 )
 
+type GreetingOutput struct {
+	Body struct {
+		Message string `json:"message" example:"Hello, world!" doc:"Greeting message"`
+	}
+}
+
 func main() {
-	http.HandleFunc("/", server.HomeHandler)
-	http.HandleFunc("/health", server.HealthHandler)
+	router := chi.NewMux()
+	api := humachi.New(router, huma.DefaultConfig("My API", "1.0.0"))
+
+	// Register GET /greeting/{name} handler.
+	huma.Get(
+		api,
+		"/greeting/{name}",
+		func(
+			ctx context.Context,
+			input *struct {
+				Name string `path:"name" maxLength:"30" example:"world" doc:"Name to greet"`
+			},
+		) (*GreetingOutput, error) {
+			resp := &GreetingOutput{}
+			resp.Body.Message = fmt.Sprintf("Hello, %s!", input.Name)
+			return resp, nil
+		},
+	)
 
 	fmt.Println("Starting server on :8080")
-	if err := http.ListenAndServe(":8080", nil); err != nil {
+	if err := http.ListenAndServe(":8080", router); err != nil {
 		log.Fatal("Server failed to start:", err)
 	}
 }
