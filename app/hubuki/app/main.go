8fef0fc57d927d9a9665eab9ce322b1f98cf94dc
shunsock <s.tsuchiya.business@gmail.com>
2025-09-21 20:32:33 +0900

    feature: add port option

--- app/hubuki/app/main.go
+++ app/hubuki/app/main.go
@@ -3,13 +3,17 @@ package main
 import (
 	"context"
 	"fmt"
-	"log"
 	"net/http"
 
 	"github.com/danielgtaylor/huma/v2"
 	"github.com/danielgtaylor/huma/v2/adapters/humachi"
+	"github.com/danielgtaylor/huma/v2/humacli"
 	"github.com/go-chi/chi/v5"
 )
+
+type Options struct {
+	Port int `help:"Port to listen on" short:"p" default:"8888"`
+}
 
 type GreetingOutput struct {
 	Body struct {
@@ -18,27 +22,31 @@ type GreetingOutput struct {
 }
 
 func main() {
-	router := chi.NewMux()
-	api := humachi.New(router, huma.DefaultConfig("My API", "1.0.0"))
+	cli := humacli.New(func(hooks humacli.Hooks, options *Options) {
+		router := chi.NewMux()
+		api := humachi.New(router, huma.DefaultConfig("My API", "1.0.0"))
 
-	// Register GET /greeting/{name} handler.
-	huma.Get(
-		api,
-		"/greeting/{name}",
-		func(
-			ctx context.Context,
-			input *struct {
-				Name string `path:"name" maxLength:"30" example:"world" doc:"Name to greet"`
+		// Register GET /greeting/{name} handler.
+		huma.Get(
+			api,
+			"/greeting/{name}",
+			func(
+				ctx context.Context,
+				input *struct {
+					Name string `path:"name" maxLength:"30" example:"world" doc:"Name to greet"`
+				},
+			) (*GreetingOutput, error) {
+				resp := &GreetingOutput{}
+				resp.Body.Message = fmt.Sprintf("Hello, %s!", input.Name)
+				return resp, nil
 			},
-		) (*GreetingOutput, error) {
-			resp := &GreetingOutput{}
-			resp.Body.Message = fmt.Sprintf("Hello, %s!", input.Name)
-			return resp, nil
-		},
-	)
+		)
 
-	fmt.Println("Starting server on :8080")
-	if err := http.ListenAndServe(":8080", router); err != nil {
-		log.Fatal("Server failed to start:", err)
-	}
+		hooks.OnStart(func() {
+			fmt.Printf("Starting server on port %d...\n", options.Port)
+			http.ListenAndServe(fmt.Sprintf(":%d", options.Port), router)
+		})
+	})
+
+	cli.Run()
 }
